de0f09173bcf32c74f4e4ccdc615a6a75cdf4599
Nicholas Tsafos <nikotsafos@Nicholass-MacBook-Pro.local>
2018-09-13 17:51:16 -0700

    Adding randomizer function

--- src/Randomizer.js
+++ src/Randomizer.js
@@ -1,112 +1,83 @@
 import React, { Component } from 'react';
-
+import Team from './Team';
+var mArray = ['Brian', 'Niko', 'Li', 'Nate', 'Andy', 'Billy', 'Andres', 'Justin', 'Devan', 'Zach', 'Yinebeb', 'Jon', 'Kelsey'];
+var teamArray = ['Brian', 'Niko', 'Li', 'Nate', 'Andy', 'Billy', 'Andres', 'Justin', 'Devan', 'Zach', 'Yinebeb', 'Jon', 'Kelsey'];
+var allTeamName = ['Bigfoot', 'Minotaur', 'Dragon', 'Pegasus', 'Chupacabra'];
+function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (array.length));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+}
+const newTeamArray = shuffleArray(mArray);
+function createOneTeam(pNum, newTeamArray) {
+  var newTeam =[];
+  if(pNum > newTeamArray.length){
+    console.log('choose a number less than', teamArray.length);
+  } else
+  {
+    for(let i = 0; i < pNum; i++) {
+      var one = newTeamArray[i];
+      newTeam.push(one);
+    }
+    newTeamArray.splice(0, pNum);
+    return newTeam;
+  }
+}
+function createAllTeam(pNum, newTeamArray){
+ const numTeam = Math.floor(teamArray.length/pNum);
+ const extraP = teamArray.length%pNum;
+ var allTeams = [];
+  for(let i = 0; i < numTeam; i++){
+    var teamnew = createOneTeam(pNum, newTeamArray);
+    allTeams.push({
+    teamName: allTeamName[i],
+    team: teamnew
+    });
+  }
+  for (let i =0; i < extraP; i++ ) {
+    allTeams[i].team.push(newTeamArray[i]);
+  }
+  return allTeams;
+}
+        var outputTeams = createAllTeam(3, newTeamArray);
 export default class Randomizer extends Component {
-
-  handleAddingStudentClick = (e) => {
-    e.stopPropagation()
-    console.log('handling adding student click');
-  }
-
-  handleFormSubmitClick = (e) => {
-    e.stopPropagation()
-    console.log('handling creating teams button');
-  }
-
   render() {
-    const teamArray = ['Brian', 'Niko', 'Li', 'Nate', 'Andy', 'Billy', 'Andres', 'Justin', 'Devan', 'Zach', 'Yinebeb', 'Jon', 'Kelsey']
+  const listOfpeople = teamArray.map((person, indx) => <li key={indx}> {person}</li> )
+  const teams = outputTeams.map(team => <Team team={team} /> )
     return (
-
       <div className='container'>
-        <h1>Randomizer stub</h1>
-        <div className="row">
-          <div className="col s12 m5 l5 card-panel red">
-              <span className="white-text">
-                <h3>Students</h3>
-                <ul>
-                  <li>List</li>
-                  <li>of</li>
-                  <li>students</li>
-                  <li>here</li>
-                </ul>
-                <form>
-                  <input type='text' className="white-text" placeholder='Add Student'></input>
-
-
-                </form>
-              </span>
-            </div>
-            <div className="col s0 m2 l2">
+      <div className="row">
+      <div className="col s12 m5 l5 card-panel red white-text">
+        <h1>Randomizer Team</h1>
+          <ul>
+            {listOfpeople}
+          </ul>
+          </div>
+              <div className="col s0 m2 l2">
               </div>
             <div className="col s12 m5 l5 card-panel red">
+              <form>
                 <span className="white-text"><h3>Num of Students per group</h3>
                 <input className="white-text" id='numOfStudent' type='number' placeholder='Number of Students per group' required></input>
                 </span>
+                    <button className="btn-large waves-effect waves-light white red-text right" type="button" name="action" onClick={this.handleFormSubmitClick}>Create teams<i className="material-icons right">send</i></button>
+                </form>
               </div>
-
-
           </div>
-
           <div className="row">
             <div className="col s12 m12 l12">
               <form>
                 <button className="btn-small waves-effect waves-light white red-text left" type="button" name="action" onClick={this.handleAddingStudentClick}>Add Student<i className="material-icons right">send</i></button>
-                <button className="btn-large waves-effect waves-light white red-text right" type="button" name="action" onClick={this.handleFormSubmitClick}>Create teams<i className="material-icons right">send</i></button>
-
               </form>
-
             </div>
           </div>
-
-          <div className="row">
-            <div className="col s12 m12 l12 card-panel red white-text">
-              <h3>List of Teams</h3>
-              <h4>team 1 name</h4>
-              <ul>
-                <li>
-                  members
-                </li>
-                <li>
-                  of
-                </li>
-                <li>
-                  the
-                </li>
-                <li>
-                  team
-                </li>
-              </ul>
-              <h4>team 2 name</h4>
-              <ul>
-                <li>
-                  members
-                </li>
-                <li>
-                  of
-                </li>
-                <li>
-                  the
-                </li>
-                <li>
-                  team
-                </li>
-              </ul>
-              <h4>team 3 name</h4>
-              <ul>
-                <li>
-                  members
-                </li>
-                <li>
-                  of
-                </li>
-                <li>
-                  the
-                </li>
-                <li>
-                  team
-                </li>
-              </ul>
-            </div>
-          </div>
+          <div className="row" >
+          <h3>TEAMS</h3>
+            {teams}
+       {/* <p>This is TEST for SHOW ! This is a paragraph</p>*/}
+        </div>
       </div>
     );
   }
---
+++ src/Team.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import './Team.css';
+
+
+
+
+class Team extends Component {
+
+  render() {
+
+  	const currentTeam = this.props.team.team.map(person => <p><a href="#">{person}</a></p> )
+
+    return (
+
+
+
+          <div>
+            <div className="card col s12 m12 l12">
+			    <div className="card-image waves-effect waves-block waves-light">
+			      <img className="activator img-size" src="https://laborplaststorage.blob.core.windows.net/imgsito/49a87d99-e999-4bde-b790-ef32015c107d.jpg" />
+			    </div>
+			    <div className="card-content">
+			      <span className="card-title activator grey-text text-darken-4">
+			      		{this.props.team.teamName} <i className="material-icons right">more_vert</i>
+			      </span>
+			      	{currentTeam}
+			    </div>
+			    <div className="card-reveal">
+			      <span className="card-title grey-text text-darken-4">
+			      Card Title<i className="material-icons right"> close</i>
+			      </span>
+			      <p>Here is some more information about this product that is only revealed once clicked on.</p>
+			    </div>
+			  </div>
+		  </div>
+
+
+    );
+  }
+}
+
+export default Team;
