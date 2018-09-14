09d40d78d84cda3fe416804054f6a226e4032f40
yuanjli <liyuanjienetwork@gmail.com>
2018-09-14 00:58:31 -0700

    help from lemon to move functions into the class in randomiser js

--- src/Randomizer.js
+++ src/Randomizer.js
@@ -8,7 +8,58 @@ var allTeamName = ['Bigfoot', 'Minotaur', 'Dragon', 'Pegasus', 'Chupacabra'];
 
 
 
-function shuffleArray(array) {
+// function shuffleArray(array) {
+//     for (let i = array.length - 1; i > 0; i--) {
+//         const j = Math.floor(Math.random() * (array.length));
+//         [array[i], array[j]] = [array[j], array[i]];
+//     }
+//     return array; 
+// }
+
+// const newTeamArray = shuffleArray(mArray); 
+
+// function createOneTeam(pNum, newTeamArray) {
+//   var newTeam =[];
+//   if(pNum > newTeamArray.length){
+//     console.log('choose a number less than', teamArray.length);
+//   } else 
+//   {
+//     for(let i = 0; i < pNum; i++) {
+//       var one = newTeamArray[i];
+//       newTeam.push(one);
+//     }
+//     newTeamArray.splice(0, pNum);
+//     return newTeam;
+//   }
+// }
+
+
+// function createAllTeam(pNum, newTeamArray){
+//  const numTeam = Math.floor(teamArray.length/pNum); 
+//  const extraP = teamArray.length%pNum; 
+//  var allTeams = [];
+//   for(let i = 0; i < numTeam; i++){
+//     var teamnew = createOneTeam(pNum, newTeamArray);
+//     allTeams.push({
+//     teamName: allTeamName[i],
+//     team: teamnew 
+//     });
+//   }
+//   for (let i =0; i < extraP; i++ ) {
+//     allTeams[i].team.push(newTeamArray[i]);
+//   }
+//   return allTeams;
+// }
+
+
+        // var outputTeams = createAllTeam(3, newTeamArray);
+
+
+
+
+export default class Randomizer extends Component {
+
+ shuffleArray(array) {
     for (let i = array.length - 1; i > 0; i--) {
         const j = Math.floor(Math.random() * (array.length));
         [array[i], array[j]] = [array[j], array[i]];
@@ -16,9 +67,9 @@ function shuffleArray(array) {
     return array; 
 }
 
-const newTeamArray = shuffleArray(mArray); 
+ newTeamArray = this.shuffleArray(mArray); 
 
-function createOneTeam(pNum, newTeamArray) {
+ createOneTeam(pNum, newTeamArray) {
   var newTeam =[];
   if(pNum > newTeamArray.length){
     console.log('choose a number less than', teamArray.length);
@@ -34,36 +85,52 @@ function createOneTeam(pNum, newTeamArray) {
 }
 
 
-function createAllTeam(pNum, newTeamArray){
+ createAllTeam(pNum, newTeamArray){
  const numTeam = Math.floor(teamArray.length/pNum); 
  const extraP = teamArray.length%pNum; 
  var allTeams = [];
   for(let i = 0; i < numTeam; i++){
-    var teamnew = createOneTeam(pNum, newTeamArray);
+    var teamnew = this.createOneTeam(pNum, newTeamArray);
     allTeams.push({
     teamName: allTeamName[i],
     team: teamnew 
     });
   }
   for (let i =0; i < extraP; i++ ) {
-    allTeams[i].team.push(newTeamArray[i]);
+    console.log(newTeamArray[i]);
+    allTeams[i].team.push(this.newTeamArray[i]);
   }
   return allTeams;
 }
 
 
-        var outputTeams = createAllTeam(3, newTeamArray);
 
+  constructor(props) {
+    super(props);
 
+    this.state = {
+      divisor: 0,
+      team: []
+    };
+  }
 
+  handleChange = (event) => {
+    console.log("this is the event's target", event.target.value);
+    this.setState({
+      [event.target.name]: event.target.value
+    });
+  }
 
-export default class Randomizer extends Component {
-
+  handleFormSubmitClick = (event) => {
+    event.preventDefault();
+    
+    this.state.team = this.createAllTeam(this.state.divisor, this.newTeamArray);
+  }
 
   render() {
 
   const listOfpeople = teamArray.map((person, indx) => <li key={indx}> {person}</li> )
-  const teams = outputTeams.map(team => <Team team={team} /> ) 
+  const teams = this.state.team.map(team => <Team team={team} /> ) 
 
     return (
 
@@ -81,11 +148,11 @@ export default class Randomizer extends Component {
               <div className="col s0 m2 l2">
               </div>
             <div className="col s12 m5 l5 card-panel red">
-              <form>  
+              <form onSubmit={this.handleFormSubmitClick}>  
                 <span className="white-text"><h3>Num of Students per group</h3>
-                <input className="white-text" id='numOfStudent' type='number' placeholder='Number of Students per group' required></input>
+                <input className="white-text" id='numOfStudent' name='divisor' type='number' placeholder='Number of Students per group' onChange={this.handleChange} required></input>
                 </span>
-                    <button className="btn-large waves-effect waves-light white red-text right" type="button" name="action" onClick={this.handleFormSubmitClick}>Create teams<i className="material-icons right">send</i></button>
+                    <button className="btn-large waves-effect waves-light white red-text right" type="submit" name="action" >Create teams<i className="material-icons right">send</i></button>
                 </form>
               </div>
           </div>
@@ -103,11 +170,13 @@ export default class Randomizer extends Component {
           <div className="row" >
           <h3>TEAMS</h3>
             {teams}
-       {/* <p>This is TEST for SHOW ! This is a paragraph</p>*/}
         </div>
+
+          <p>This is TEST for SHOW ! This is a paragraph</p>
 
       </div>
     );
   }
 }
+
 
